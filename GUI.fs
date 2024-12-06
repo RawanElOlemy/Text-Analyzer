979d5cb25056fc1a282bc81cc61d0bf85733c098
heka2002 <120687474+heka2002@users.noreply.github.com>
2024-12-06 21:01:48 +0200

    Add files via upload

--- GUI.fs
+++ GUI.fs
@@ -1,6 +1,8 @@
 ﻿open System
+open System.IO
 open System.Drawing
 open System.Windows.Forms
+open TextAnalyzer.LoadTxt
 
 let form = new Form(Text = "Text Analyzer", Width = 600, Height = 400, StartPosition = FormStartPosition.CenterScreen)
 
@@ -15,8 +17,32 @@ let textBox = new TextBox(
 textBoxPanel.Controls.Add(textBox)
 form.Controls.Add(textBoxPanel)
 
-let bottomPanel = new Panel(Dock = DockStyle.Bottom, Height = 60)
-form.Controls.Add(bottomPanel)
+let buttonPanel = new Panel(Dock = DockStyle.Bottom, Height = 60)
+form.Controls.Add(buttonPanel)
+
+
+let loadButton = TextAnalyzer.LoadTxt.createLoadButton()  
+buttonPanel.Controls.Add(loadButton)
+
+let centerButton () =
+    loadButton.Location <- Point(
+        (buttonPanel.Width - loadButton.Width) / 2,  (buttonPanel.Height - loadButton.Height) / 2 )
+
+centerButton()
+
+buttonPanel.Resize.Add(fun _ -> centerButton())
+
+let openFileDialog = new OpenFileDialog(Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*")
+
+loadButton.Click.Add(fun _ ->
+    if openFileDialog.ShowDialog() = DialogResult.OK then
+        let filePath = openFileDialog.FileName
+        try
+            let fileContent = File.ReadAllText(filePath)
+            textBox.Text <- fileContent
+        with
+        | ex -> MessageBox.Show($"Error reading file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error) |> ignore
+)
 
 [<STAThread>]
 do Application.Run(form)
---
+++ LoadTxt.fs
@@ -0,0 +1,16 @@
+﻿namespace TextAnalyzer  
+open System.Drawing
+open System.Windows.Forms
+
+module LoadTxt =  
+    let createLoadButton() =
+        let loadButton = new Button(
+            Text = "Load .txt File", 
+            Width = 150, 
+            Height = 40, 
+            FlatStyle = FlatStyle.Flat, 
+            BackColor = Color.FromArgb(222, 49, 99),  
+            ForeColor = Color.White,  
+            Font = new Font("Segoe UI", 10.0f, FontStyle.Bold)
+        )
+        loadButton
